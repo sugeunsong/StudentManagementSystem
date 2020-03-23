810118eb603d96b80feeccd9184d70c36c035a35
sugeunsong <62562024+sugeunsong@users.noreply.github.com>
2020-03-24 02:02:23 +0900

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+		System.out.println("Hello, world!");
+		System.out.println("I am a new java programmer");
+	}
+
+}
